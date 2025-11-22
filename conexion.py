96d34fb91559dbeef87a7a5d0c16dc72b9453a02
IsabelNavarrete04 <123046270@upq.edu.mx>
2025-11-22 14:23:45 -0600

    ignorar

--- conexion.py
+++ conexion.py
@@ -1,9 +1,14 @@
+from dotenv import load_dotenv
+import os
 import mysql.connector
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host='localhost',
-        user='root',          
-        password='',         
-        database='pi'
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT")
     )
